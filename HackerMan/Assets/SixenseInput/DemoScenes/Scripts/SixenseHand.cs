07d07b8a00728fa23f2cf781eeb6a24b21587d59
magicdustin <magicdustin14@gmail.com>
2016-01-29 20:26:40 -0800

    Added input methods for hydra and get hand velocity
    Set up functions to handle simple hydra input.
    
    Added function to calculate velocity for left and right hydra hands to
    be applied to throw objects.

--- HackerMan/Assets/SixenseInput/DemoScenes/Scripts/SixenseHand.cs
+++ HackerMan/Assets/SixenseInput/DemoScenes/Scripts/SixenseHand.cs
@@ -12,100 +12,86 @@ public class SixenseHand : MonoBehaviour
 	public SixenseHands	m_hand;
 	public SixenseInput.Controller m_controller = null;
 
-	Animator 	m_animator;
-	float 		m_fLastTriggerVal;
-	Vector3		m_initialPosition;
+    //Animator 	m_animator;
+    Vector3 LVelocity, RVelocity;
+	Vector3		m_initialPosition, LprevPosition, RprevPosition;
 	Quaternion 	m_initialRotation;
 
 
 	protected void Start() 
 	{
 		// get the Animator
-		m_animator = gameObject.GetComponent<Animator>();
+		//m_animator = gameObject.GetComponent<Animator>();
 		m_initialRotation = transform.localRotation;
 		m_initialPosition = transform.localPosition;
 	}
 
 
-	protected void Update()
-	{
-		if ( m_controller == null )
-		{
-			m_controller = SixenseInput.GetController( m_hand );
-		}
+    protected void Update()
+    {
+        if (m_controller == null)
+        {
+            m_controller = SixenseInput.GetController(m_hand);
+        }
 
-		else if ( m_animator != null )
-		{
-			UpdateHandAnimation();
-		}
+
+        LVelocity = CalcVelocity(0);
+        RVelocity = CalcVelocity(1);
+
+        
+        for (int i = 0; i < 2; i++)
+        { 
+            
+            HandleInput(i);
+        }
 	}
 	
 	
 	// Updates the animated object from controller input.
-	protected void UpdateHandAnimation()
+	protected void HandleInput(int i)
 	{
-		// Point
-		if ( m_hand == SixenseHands.RIGHT ? m_controller.GetButton(SixenseButtons.ONE) : m_controller.GetButton(SixenseButtons.TWO) )
-		{
-			m_animator.SetBool( "Point", true );
-		}
-		else
-		{
-			m_animator.SetBool( "Point", false );
-		}
-		
-		// Grip Ball
-		if ( m_hand == SixenseHands.RIGHT ? m_controller.GetButton(SixenseButtons.TWO) : m_controller.GetButton(SixenseButtons.ONE)  )
-		{
-			m_animator.SetBool( "GripBall", true );
-		}
-		else
-		{
-			m_animator.SetBool( "GripBall", false );
-		}
-				
-		// Hold Book
-		if ( m_hand == SixenseHands.RIGHT ? m_controller.GetButton(SixenseButtons.THREE) : m_controller.GetButton(SixenseButtons.FOUR) )
-		{
-			m_animator.SetBool( "HoldBook", true );
-		}
-		else
-		{
-			m_animator.SetBool( "HoldBook", false );
-		}
-				
-		// Fist
-		float fTriggerVal = m_controller.Trigger;
-		fTriggerVal = Mathf.Lerp( m_fLastTriggerVal, fTriggerVal, 0.1f );
-		m_fLastTriggerVal = fTriggerVal;
-		
-		if ( fTriggerVal > 0.01f )
-		{
-			m_animator.SetBool( "Fist", true );
-		}
-		else
-		{
-			m_animator.SetBool( "Fist", false );
-		}
-		
-		m_animator.SetFloat("FistAmount", fTriggerVal);
-		
-		// Idle
-		if ( m_animator.GetBool("Fist") == false &&  
-			 m_animator.GetBool("HoldBook") == false && 
-			 m_animator.GetBool("GripBall") == false && 
-			 m_animator.GetBool("Point") == false )
-		{
-			m_animator.SetBool("Idle", true);
-		}
-		else
-		{
-			m_animator.SetBool("Idle", false);
-		}
+        
+            if(SixenseInput.Controllers[i].GetButton(SixenseButtons.TRIGGER))
+            {
+                //if left controller presses trigger down
+                if(i == 0)
+                {
+                print(LVelocity + " left");
+                
+            }
+                //if right controller presses trigger down
+                else
+                {
+                print(RVelocity + "right");
+            }
+            
+
+
+
+        }
 	}
 
+    Vector3 CalcVelocity(int i)
+    {
+        //finish velocity work!!!
+        Vector3 velocity; 
 
-	public Quaternion InitialRotation
+        if(i == 0)
+        {
+            velocity = (SixenseInput.Controllers[i].Position - LprevPosition) / Time.deltaTime;
+            LprevPosition = SixenseInput.Controllers[i].Position;
+        }
+        else
+        {
+            velocity = (SixenseInput.Controllers[i].Position - RprevPosition) / Time.deltaTime;
+            RprevPosition = SixenseInput.Controllers[i].Position;
+        }
+        
+        return velocity;
+    }
+
+
+    public Quaternion InitialRotation
 	{
 		get { return m_initialRotation; }
 	}
