41c05e150d3e830ff0278ff2e33235f7cb00345d
magicdustin <magicdustin14@gmail.com>
2016-02-03 16:25:23 -0800

    added battery holder

---
+++ HackerMan/Assets/batteryHolder.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class batteryHolder : MonoBehaviour {
+
+    GameObject battery = null;
+    public GameObject batterMold, Lightbulb, spotlight;
+
+	// Use this for initialization
+	void Start () {
+        Lightbulb.gameObject.SetActive(false);
+        spotlight.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (battery != null)
+        {
+
+            battery.transform.position = batterMold.transform.position;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider col)
+    {
+        if(col.name == "Battery")
+        {
+            battery = col.gameObject;
+            battery.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            battery.GetComponent<Rigidbody>().useGravity = false;
+            battery.transform.rotation = batterMold.transform.rotation;
+            Lightbulb.SetActive(true);
+            spotlight.SetActive(true);
+        }
+    }
+}
